8344c75b42cf87caefaa5752173279ef5569bb90
Ivan Senchukov <Iasench@yandex.ru>
2020-06-13 12:45:43 +0300

    module1-task2 - начинаем программировать  - Исправление замечаний код-ревью

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app.jsx";
 
-const Settings = {
-  ERRORS_COUNT: 3
+const settings = {
+  errorsCount: 3
 };
 
 ReactDOM.render(
-    <App errorsCount={Settings.ERRORS_COUNT}/>,
+    <App errorsCount={settings.errorsCount}/>,
     document.querySelector(`#root`)
 );
