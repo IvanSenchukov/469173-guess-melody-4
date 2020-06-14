2563432d888abf13f57d8d2244534a6c4abd4e04
Ivan Senchukov <Iasench@yandex.ru>
2020-06-14 23:10:29 +0300

    module2-task1 - Валидируй, как никогда не валидировал  - Настроены propTypes для компонентов App и WelcomeScreen

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WelcomeScreen from "./welcome-screen.jsx";
 
 const App = (props) => {
   return (
     <WelcomeScreen errorsCount={props.errorsCount}/>
   );
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired
 };
 
 export default App;
