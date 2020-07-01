6e0e00ace0fe89fdec808acb31188656f704e42e
Ivan Senchukov <Iasench@yandex.ru>
2020-07-01 14:00:14 +0300

    module3-task1 Настройка Роутера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import QuestionArtist from "../question-artist/question-artist.jsx";
+import QuestionGenre from "../question-genre/question-genre.jsx";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = (props) => {
 
   const startGameCallback = () => {};
   return (
-    <WelcomeScreen errorsCount={props.errorsCount} onWelcomeButtonClick={startGameCallback}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={props.errorsCount} onWelcomeButtonClick={startGameCallback}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist/>
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
