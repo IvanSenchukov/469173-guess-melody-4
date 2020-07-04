f9a628810d93ecd8ec3fd27fae757693546cca6a
Ivan Senchukov <Iasench@yandex.ru>
2020-07-05 00:51:13 +0300

    module3-task1 - компонент QuestionArtist - snapshot test.

---
+++ src/components/question-artist/question-artist.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuestionArtist from "./question-artist";
+import {QUESTION_TYPE_ARTIST} from "../../mocks/questions";
+
+const mock = {
+  type: QUESTION_TYPE_ARTIST,
+  artist: `Краснознаменная дивизия имени моей бабушки`,
+  audio: {
+    source: ``
+  },
+  guesses: [
+    {
+      artistName: `Пелагея`,
+      artistPicture: `http://placehold.it/134x134`
+    },
+    {
+      artistName: `Краснознаменная дивизия имени моей бабушки`,
+      artistPicture: `http://placehold.it/134x134`
+    },
+    {
+      artistName: `Lorde`,
+      artistPicture: `http://placehold.it/134x134`
+    }
+  ]
+};
+
+
+it(`should renders correctly`, function () {
+  const tree = renderer
+    .create(<QuestionArtist question={mock} errorsCount={3} onAnswer={jest.fn()}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
