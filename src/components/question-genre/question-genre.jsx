00fcdc99ebbc0c1990c2d9bd92267676ad422a27
Ivan Senchukov <Iasench@yandex.ru>
2020-07-05 00:10:06 +0300

    module3-task1 - компонент "угадай жанр" - добавлено состояние. Передан колбэк.

--- src/components/question-genre/question-genre.jsx
+++ src/components/question-genre/question-genre.jsx
@@ -2,77 +2,93 @@ import React from 'React';
 import PropTypes from 'prop-types';
 import {GENRE_BLUES, GENRE_INDIE_ROCK, GENRE_JAZZ} from "../../mocks/questions";
 
-const QuestionGenre = (props) => {
+class QuestionGenre extends React.PureComponent {
 
-  function generateTitle(questionGenre) {
-
-    let genre;
-
-    switch (questionGenre) {
-      case GENRE_INDIE_ROCK : {
-        genre = `инди-рок`;
-        break;
-      }
-      case GENRE_BLUES: {
-        genre = `блюз`;
-        break;
-      }
-      case GENRE_JAZZ: {
-        genre = `джаз`;
-        break;
-      }
-    }
-
-    return `Выберите ${genre} треки`;
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [false, false, false, false]
+    };
   }
 
-  return (
-    <section className="game game--genre">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"></img>
-        </a>
+  render() {
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transfrmOrigin: `center`}}/>
-        </svg>
+    function generateTitle(questionGenre) {
 
-        <div className="game__mistakes">
-          {Array.from(Array(props.errorsCount)).map((mistake, number) => {
-            return (
-              <div className="wrong" key={`mistake-${number}`}></div>
-            );
-          })}
-        </div>
-      </header>
+      let genre;
 
-      <section className="game__screen">
-        <h2 className="game__title">{generateTitle(props.question.genre)}</h2>
-        <form className="game__tracks">
+      switch (questionGenre) {
+        case GENRE_INDIE_ROCK : {
+          genre = `инди-рок`;
+          break;
+        }
+        case GENRE_BLUES: {
+          genre = `блюз`;
+          break;
+        }
+        case GENRE_JAZZ: {
+          genre = `джаз`;
+          break;
+        }
+      }
 
-          {props.question.guesses.map((guess, index) => {
-            const answerId = `answer-${index}`;
-            return (
-              <div className="track" key={answerId}>
-                <button className="track__button track__button--play" type="button"></button>
-                <div className="track__status">
-                  <audio src={guess.audio.source}></audio>
+      return `Выберите ${genre} треки`;
+    }
+
+    return (
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"></img>
+          </a>
+
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transfrmOrigin: `center`}}/>
+          </svg>
+
+          <div className="game__mistakes">
+            {Array.from(Array(this.props.errorsCount)).map((mistake, number) => {
+              return (
+                <div className="wrong" key={`mistake-${number}`}></div>
+              );
+            })}
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">{generateTitle(this.props.question.genre)}</h2>
+          <form className="game__tracks">
+
+            {this.props.question.guesses.map((guess, index) => {
+              const answerId = `answer-${index}`;
+              return (
+                <div className="track" key={answerId}>
+                  <button className="track__button track__button--play" type="button"></button>
+                  <div className="track__status">
+                    <audio src={guess.audio.source}></audio>
+                  </div>
+                  <div className="game__answer">
+                    <input className="game__input visually-hidden" type="checkbox" checked={this.state.answers[index]} name="answer" value={answerId} id={answerId}></input>
+                    <label className="game__check" htmlFor={answerId}>Отметить</label>
+                  </div>
                 </div>
-                <div className="game__answer">
-                  <input className="game__input visually-hidden" type="checkbox" checked={guess.selected} name="answer" value={answerId} id={answerId}></input>
-                  <label className="game__check" htmlFor={answerId}>Отметить</label>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
 
-          <button className="game__submit button" type="submit" onClick={props.onAnswer}>Ответить</button>
-        </form>
+            <button
+              className="game__submit button"
+              type="submit"
+              onClick={() => {
+                this.props.onAnswer(this.state.answers);
+              }}>Ответить</button>
+          </form>
+        </section>
       </section>
-    </section>
-  );
-};
+    );
+  }
+
+}
 
 QuestionGenre.propTypes = {
   question: PropTypes.shape(
