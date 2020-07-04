fe1df8a3a4443ad0f93eaa0d80aea83e596122c9
Ivan Senchukov <Iasench@yandex.ru>
2020-07-05 01:29:58 +0300

    module3-task1 - компонент QuestionGenre - snapshot test.

---
+++ src/components/question-genre/question-genre.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuestionGenre from "./question-genre";
+import {GENRE_BLUES, GENRE_INDIE_ROCK, GENRE_JAZZ, QUESTION_TYPE_GENRE} from "../../mocks/questions";
+
+const mock = {
+  question: {
+    type: QUESTION_TYPE_GENRE,
+    genre: GENRE_INDIE_ROCK,
+    guesses: [
+      {
+        audio: {
+          source: ``
+        },
+        genre: GENRE_BLUES,
+        selected: false
+      },
+      {
+        audio: {
+          source: ``
+        },
+        genre: GENRE_JAZZ,
+        selected: false
+      },
+      {
+        audio: {
+          source: ``
+        },
+        genre: GENRE_INDIE_ROCK,
+        selected: true
+      },
+      {
+        audio: {
+          source: ``
+        },
+        genre: GENRE_INDIE_ROCK,
+        selected: false
+      }
+    ]
+  }
+
+};
+
+
+it(`should renders correctly`, function () {
+  const tree = renderer
+    .create(<QuestionGenre question={mock.question} errorsCount={2} onAnswer={jest.fn()}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
